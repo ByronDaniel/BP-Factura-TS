e249972044ac94889db2239c96302c59c9eb0fab
Byron Delgado <byrondanipm@gmail.com>
2022-06-09 20:38:12 -0500

    Ejericio Factura

--- dist/index.js
+++ dist/index.js
@@ -1,42 +1,88 @@
 "use strict";
-class Factura {
+class Products {
     constructor() {
+        //metodo para generar codigo random
         this.newUID = () => Math.random().toString(36).slice(2);
-        this.productos = [];
-        this.total = 0;
-        this.subtotal = 0;
-        this.iva = 0;
+        this.RegisteredProducts = [];
+        this.Products = [];
+        this.Total = {
+            totalItems: 0,
+            subTotal: 0,
+            total: 0,
+        };
     }
-    agregarProducto(producto) {
-        let totalProductos = producto.precio * producto.cantidad;
-        this.subtotal += totalProductos;
-        producto.precioTotal = totalProductos;
-        producto.codigo = this.newUID();
-        this.productos.push(producto);
+    //metodo para agregar productos a la tienda
+    agregarProductoInfo(name, price) {
+        let newProduct = {
+            cod: this.newUID(),
+            name,
+            price,
+        };
+        let existProduct = this.obtenerInfoProducto(name);
+        if (existProduct.cod == "") {
+            this.RegisteredProducts.push(newProduct);
+        }
     }
-    verProductos() {
-        this.productos.forEach((producto) => {
-            var _a;
-            console.log(`Cod: ${producto.codigo}, Cant: ${producto.cantidad}, prod: ${producto.nombre}, precio: ${producto.precio.toFixed(2)}, total ${(_a = producto.precioTotal) === null || _a === void 0 ? void 0 : _a.toFixed(2)}`);
+    //metodo para obtener informacion de un producto, codigo y precio
+    obtenerInfoProducto(name) {
+        let product = {
+            name: "",
+            cod: "",
+            price: 0,
+        };
+        this.RegisteredProducts.forEach((e) => {
+            if (e.name == name) {
+                product = e;
+            }
         });
+        return product;
     }
+    //metodo para agregar producto a la factura
+    agregarProducto(cant, producto) {
+        let infoProducto = this.obtenerInfoProducto(producto);
+        if (infoProducto.cod != "" && infoProducto.cod != null) {
+            this.Total.totalItems += cant;
+            this.Total.subTotal += infoProducto.price * cant;
+            let productoGenerado = {
+                name: producto,
+                cant: cant,
+                price: infoProducto.price,
+                cod: infoProducto.cod,
+            };
+            this.Products.push(productoGenerado);
+            console.log(`Producto Agregado: ${producto} - Cantidad: ${cant}`);
+        }
+        else {
+            console.log(`Producto No Agregado - No existe en el sistema: ${producto} - Cantidad: ${cant}`);
+        }
+    }
+    //metodo para calcular el total (subtotal + iva)
     calcularTotal() {
-        this.iva = this.subtotal * 0.12;
-        this.total = this.subtotal + this.iva;
+        let total = this.Total.subTotal + this.Total.subTotal * 0.12;
+        this.Total.total = parseFloat(total.toFixed(2));
     }
+    //metodo para imprimir los detalles de la factura
     imprimir() {
         this.calcularTotal();
-        console.log("----- Factura -----");
-        this.verProductos();
-        console.log(`Subtotal: ${this.subtotal.toFixed(2)}`);
-        console.log(`IVA 12%: ${(this.iva).toFixed(2)}`);
-        console.log(`Total: ${this.total.toFixed(2)}`);
+        console.log("************\n************\n************\n");
+        console.log(`Products Factura => `, this.Products);
+        console.log(`Total Factura => `, this.Total);
     }
 }
-const factura = new Factura();
-factura.agregarProducto({ cantidad: 2, nombre: "Aceite", precio: 1.5 });
-factura.agregarProducto({ cantidad: 3, nombre: "Jamon", precio: 3 });
-factura.agregarProducto({ cantidad: 1, nombre: "Leche", precio: 2 });
-factura.agregarProducto({ cantidad: 5, nombre: "Pan", precio: 0.15 });
-factura.agregarProducto({ cantidad: 1, nombre: "Sicafe", precio: 0.40 });
+//Ejecucion del Programa
+const factura = new Products();
+//Agrego Productos al Sistema
+factura.agregarProductoInfo("Pan", 0.18);
+factura.agregarProductoInfo("Aceite", 1.5);
+factura.agregarProductoInfo("Leche", 1);
+factura.agregarProductoInfo("Atun", 1.5);
+factura.agregarProductoInfo("Mortadela", 1);
+factura.agregarProductoInfo("Mortadela", 1);
+//Agrego Productos a la Factura
+factura.agregarProducto(2, "Aceite");
+factura.agregarProducto(3, "Jamon");
+factura.agregarProducto(1, "Leche");
+factura.agregarProducto(5, "Pan");
+factura.agregarProducto(1, "Sicafe");
+//Imprimo Factura
 factura.imprimir();
--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,113 @@
-class Factura{
-    private productos : IProducto[];
-    private total : number;
-    private subtotal : number;
-    private iva : number;
-    
-    constructor(){
-        this.productos = [];
-        this.total = 0;
-        this.subtotal = 0;
-        this.iva = 0;
-    }
-    agregarProducto(producto : IProducto) : void {
-        let totalProductos = producto.precio * producto.cantidad;
-        this.subtotal += totalProductos;
-        producto.precioTotal = totalProductos;
-        producto.codigo = this.newUID();
-        this.productos.push(producto);
-    }
-    
-    private verProductos() : void {
-        this.productos.forEach((producto)=>{
-            console.log(`Cod: ${producto.codigo}, Cant: ${producto.cantidad}, prod: ${producto.nombre}, precio: ${producto.precio.toFixed(2)}, total ${producto.precioTotal?.toFixed(2)}`);
-        })
-    }
+class Products {
+  private RegisteredProducts: RegisteredProducts[];
+  private Products: Factura[];
+  private Total: Total;
 
-    private calcularTotal() : void {
-        this.iva = this.subtotal * 0.12;
-        this.total = this.subtotal + this.iva;
+  constructor() {
+    this.RegisteredProducts = [];
+    this.Products = [];
+    this.Total = {
+      totalItems: 0,
+      subTotal: 0,
+      total: 0,
+    };
+  }
+
+  //metodo para agregar productos a la tienda
+  agregarProductoInfo(name: string, price: number) {
+    let newProduct: RegisteredProducts = {
+      cod: this.newUID(),
+      name,
+      price,
+    };
+    let existProduct: RegisteredProducts = this.obtenerInfoProducto(name);
+    if (existProduct.cod == "") {
+      this.RegisteredProducts.push(newProduct);
     }
-    
-    imprimir() : void {
-        this.calcularTotal();
-        console.log("----- Factura -----")
-        this.verProductos();
-        console.log(`Subtotal: ${this.subtotal.toFixed(2)}`);
-        console.log(`IVA 12%: ${(this.iva).toFixed(2)}`);
-        console.log(`Total: ${this.total.toFixed(2)}`);
+  }
+
+  //metodo para obtener informacion de un producto, codigo y precio
+  obtenerInfoProducto(name: string): any {
+    let product: RegisteredProducts = {
+      name: "",
+      cod: "",
+      price: 0,
+    };
+    this.RegisteredProducts.forEach((e) => {
+      if (e.name == name) {
+        product = e;
+      }
+    });
+    return product;
+  }
+
+  //metodo para generar codigo random
+  private newUID = (): string => Math.random().toString(36).slice(2);
+
+  //metodo para agregar producto a la factura
+  agregarProducto(cant: number, producto: string): void {
+    let infoProducto: RegisteredProducts = this.obtenerInfoProducto(producto);
+    if (infoProducto.cod != "" && infoProducto.cod != null) {
+      this.Total.totalItems += cant;
+      this.Total.subTotal += infoProducto.price * cant;
+      let productoGenerado: Factura = {
+        name: producto,
+        cant: cant,
+        price: infoProducto.price,
+        cod: infoProducto.cod,
+      };
+      this.Products.push(productoGenerado);
+      console.log(`Producto Agregado: ${producto} - Cantidad: ${cant}`);
+    } else {
+      console.log(
+        `Producto No Agregado - No existe en el sistema: ${producto} - Cantidad: ${cant}`
+      );
     }
+  }
+  //metodo para calcular el total (subtotal + iva)
+  private calcularTotal(): void {
+    let total = this.Total.subTotal + this.Total.subTotal * 0.12;
+    this.Total.total = parseFloat(total.toFixed(2));
+  }
+  //metodo para imprimir los detalles de la factura
+  imprimir(): void {
+    this.calcularTotal();
+    console.log("************\n************\n************\n");
+    console.log(`Products Factura => `, this.Products);
+    console.log(`Total Factura => `, this.Total);
+  }
 }
-class AdminStore{
-    private productosInfo : IProductoInfo[];
-    constructor(){
-        this.productosInfo = [];
-    }
-    agregarProducto(productoInfo : IProductoInfo){
-        this.productosInfo.forEach(producto => {
-            if(producto.nombre.toUpperCase() == productoInfo.nombre.toUpperCase()){
-                console.log("Ya existe el producto");
-            }else{
-                let codigo : string = this.newUID();
-                this.productosInfo.push(productoInfo);
-            }
-        });
-    }
-    private newUID = (): string => Math.random().toString(36).slice(2)
+//Interfaces
+interface Factura extends RegisteredProducts {
+  cant: number;
 }
-interface IProducto extends IProductoInfo{
-    cantidad: number,
-    precioTotal?: number
+interface RegisteredProducts {
+  cod?: string;
+  name: string;
+  price: number;
+}
+interface Total {
+  totalItems: number;
+  subTotal: number;
+  total: number;
 }
 
-interface IProductoInfo{
-    codigo?: string,
-    nombre : string,
-    precio? : number,
-}
+//Ejecucion del Programa
+const factura = new Products();
+//Agrego Productos al Sistema
+factura.agregarProductoInfo("Pan", 0.18);
+factura.agregarProductoInfo("Aceite", 1.5);
+factura.agregarProductoInfo("Leche", 1);
+factura.agregarProductoInfo("Atun", 1.5);
+factura.agregarProductoInfo("Mortadela", 1);
+factura.agregarProductoInfo("Mortadela", 1);
 
-const factura = new Factura();
+//Agrego Productos a la Factura
+factura.agregarProducto(2, "Aceite");
+factura.agregarProducto(3, "Jamon");
+factura.agregarProducto(1, "Leche");
+factura.agregarProducto(5, "Pan");
+factura.agregarProducto(1, "Sicafe");
 
-factura.agregarProducto({cantidad: 2, nombre : "Aceite"});
-factura.agregarProducto({cantidad: 3, nombre : "Jamon"});
-factura.agregarProducto({cantidad: 1, nombre : "Leche"});
-factura.agregarProducto({cantidad: 5, nombre : "Pan"});
-factura.agregarProducto({cantidad: 1, nombre : "Sicafe"});
+//Imprimo Factura
 factura.imprimir();
